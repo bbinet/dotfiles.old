5a02ee41ad0d84891850162ebdbdc191088d7a2f
Bruno Binet <bruno.binet@gmail.com>
2012-04-26 12:46:12 +0200

    update install.py script

--- install.py
+++ install.py
@@ -3,13 +3,32 @@ import os
 import fnmatch
 import shutil
 
+home = os.path.expanduser('~')
+exclude = ['*.sw*', '*.un~', '.git', '.gitignore', '.gitmodules', '[!.]*']
+
 def run(cmd):
     """simple wrapper around os.system to log the command to sys.stdout"""
     print 'running: ' + cmd
     os.system(cmd)
 
-exclude = ['*.sw*', '*.un~', '.git', '.gitignore', '.gitmodules', '[!.]*']
-home = os.path.expanduser('~')
+vburrito_dir = os.path.join(home, '.venvburrito')
+vburrito = os.path.join(vburrito_dir, 'bin', 'virtualenv-burrito')
+
+cmd = vburrito + ' upgrade'
+if not os.path.exists(os.path.join(vburrito_dir)):
+    os.makedirs(os.path.join(vburrito_dir, 'lib', 'python'))
+    os.makedirs(os.path.join(vburrito_dir, 'bin'))
+    cmd += ' firstrun'
+shutil.copyfile('virtualenv-burrito/virtualenv-burrito.py', vburrito)
+os.chmod(vburrito, 0755)
+run(cmd)
+
+venv_global = os.path.join(home, '.virtualenvs/global')
+if not os.path.exists(venv_global):
+    run('source %s && mkvirtualenv global' % \
+            os.path.join(vburrito_dir, 'startup.sh'))
+
+run(os.path.join(venv_global, 'bin', 'pip') + ' install -r requirements.txt')
 
 for f in os.listdir('.'):
     if not any(fnmatch.fnmatch(f, p) for p in exclude):
@@ -20,22 +39,3 @@ for f in os.listdir('.'):
             shutil.rmtree(path)
         print 'create link for %s' % (path)
         os.symlink(os.path.abspath(f), path)
-
-vburrito = os.path.join(home, 'virtualenv-burrito')
-shutil.copyfile('virtualenv-burrito/virtualenv-burrito.py', vburrito)
-os.chmod(vburrito, 0755)
-
-cmd = vburrito + ' upgrade'
-vburrito_dir = os.path.join(home, '.venvburrito')
-if not os.path.exists(os.path.join(vburrito_dir)):
-    os.makedirs(os.path.join(vburrito_dir, 'lib', 'python'))
-    os.makedirs(os.path.join(vburrito_dir, 'bin'))
-    cmd += ' firstrun'
-run(cmd)
-
-venv_global = os.path.join(home, '.virtualenvs/global')
-if not os.path.exists(venv_global):
-    run('source %s && mkvirtualenv global' % \
-            os.path.join(vburrito_dir, 'startup.sh'))
-
-run(os.path.join(venv_global, 'bin', 'pip') + ' install -r requirements.txt')
