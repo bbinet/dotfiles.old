ef1af015a159a0270b8cc6a318e9573965804341
Bruno Binet <bruno.binet@gmail.com>
2012-04-26 11:14:01 +0200

    automatically install venv-burrito in the install.py script

--- install.py
+++ install.py
@@ -15,3 +15,16 @@ for f in os.listdir('.'):
             shutil.rmtree(path)
         os.symlink(os.path.abspath(f), path)
         print 'create link for %s' % (path)
+
+vburrito = os.path.join(home, 'virtualenv-burrito')
+shutil.copyfile('virtualenv-burrito/virtualenv-burrito.py', vburrito)
+os.chmod(vburrito, 0755)
+
+cmd = vburrito + ' upgrade'
+vburrito_dir = os.path.join(home, '.venvburrito')
+if not os.path.exists(os.path.join(vburrito_dir)):
+    os.makedirs(os.path.join(vburrito_dir, 'lib', 'python'))
+    os.makedirs(os.path.join(vburrito_dir, 'bin'))
+    cmd += ' firstrun'
+print 'running: ' + cmd
+os.system(cmd)
